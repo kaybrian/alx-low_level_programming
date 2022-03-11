3fff10f17a9f3a1c09249aecad5389ab3f4ec4a3
Kayongo brian <kayonbrian@gmail.com>
2022-03-11 11:18:22 +0300

    code to print  lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include<stdio.h>
+
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int num;
+
+for(num=0;num<10;num++)
+{
+printf("%d",num);
+}
+printf("\n");
+
+}
