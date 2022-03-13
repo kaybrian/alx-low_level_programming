28924ee5c1ebab4dbc41ee239a3c06d071a1aa28
Kayongo brian <kayonbrian@gmail.com>
2022-03-14 00:33:41 +0300

    added the styles to the work

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include<stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 int ch;
 
-for(ch='a';ch<='z';ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-if(ch=='q' || ch=='e'){
-    continue;
+if (ch == 'q' || ch == 'e'){
+continue;
 }
 putchar(ch);
-    
+
 }
 putchar('\n');
+return (0);
 }
