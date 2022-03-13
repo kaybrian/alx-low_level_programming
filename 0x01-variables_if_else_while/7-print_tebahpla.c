13df0eecabd0cd6e3503d220e76ae31d2eae0fe6
Kayongo brian <kayonbrian@gmail.com>
2022-03-14 00:41:23 +0300

    added betty to 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include<stdio.h>
-
-
-/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 int ch;
 
-for(ch='z';ch>='a';ch--)
+for (ch = 'z' ; ch >= 'a' ; ch--)
 {
 putchar(ch);
 }
 
 putchar('\n');
+return (0);
+
 }
