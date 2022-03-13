633775ddd94b3e612a5bef0db1993d109c393081
Kayongo brian <kayonbrian@gmail.com>
2022-03-14 00:12:58 +0300

    editied 1-last_digits to follow betty rules

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include<stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 
-
-/* betty style doc for function main goes there */
 int main(void)
 {
-    int last_number;
-	int n;
+int last_num;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_num = n % 10;
+if (last_num > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
+}
+else if (last_num == 0)
+{
+printf("Last digit of %d is 0 and is 0\n", n);
+}
+else if (last_num < 6 && last_num != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
+}
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-    last_number = n % 10;
-    if (last_number > 5)
-    {
-        printf("Last digit of %d is %d and is greater than 5\n", n, last_number);
-    }
-    else if (last_number == 0)
-    {
-        printf("Last digit of %d is 0 and is 0\n",n);
-    }
-    else if (last_number < 6 && last_number !=0)
-    {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_number);
-    }
-    
-	return (0);
+return (0);
 }
