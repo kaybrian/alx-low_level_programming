e11813197e4dc8b03635b5683e51d16caaa76d14
Kayongo brian <kayonbrian@gmail.com>
2022-03-17 20:19:48 +0300

    drawing a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,18 @@
 void print_line(int n)
 {
 int x;
-if (n > 0)
+
+if (n <= 0)
 {
-for (x = 0; x <= n; x++)
-{
-_putchar('_');
-}
-}
 _putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+_putchar(95);
+}
+
+_putchar('\n');
+}
 }
