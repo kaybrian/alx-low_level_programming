6dab9c0138455be1113711d1c7e6ed63821b6b53
Kayongo brian <kayonbrian@gmail.com>
2022-03-17 20:42:22 +0300

    aded the fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,40 @@
-#include<stdio.h>
-/**
- * _isdigit - check if a character is a digit
- * @c: character to check
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
+#include <stdio.h>
 
+/**
+ * main - Prints a Fizz Buzz program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
+int x;
+
+for (x = 1; x <= 100; x++)
 {
-if (i % 15 == 0)
+if ((x % 3 == 0) && (x % 5 == 0))
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
-else if (i % 3 == 0)
+else if (x % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
-else if (i % 5 == 0)
+else if (x % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", x);
+}
+
+if (x != 100)
+{
+printf(" ");
 }
 }
+
 printf("\n");
+
+return (0);
 }
