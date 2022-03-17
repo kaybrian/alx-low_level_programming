cc3d2e79d9658bc6dc3237366f1d8a8f34f778ea
Kayongo brian <kayonbrian@gmail.com>
2022-03-17 20:21:27 +0300

    drawing a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_line - draws a straight line in the terminal.
- *
+ *@n: the number of times the character will be printed.
  * Return: void
  */
 void print_line(int n)
