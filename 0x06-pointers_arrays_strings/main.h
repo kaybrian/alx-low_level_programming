d30e4af2d1639af8a87f5346a5a785b1cd8ac646
Kayongo brian <kayonbrian@gmail.com>
2022-03-24 14:15:48 +0300

    function that prints an integer.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,4 +12,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
 #endif /* MAIN_H */ 
