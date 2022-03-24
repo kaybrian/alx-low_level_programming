b823c8bb4e9c2b4faac6c4164b20137580d1e38c
Kayongo brian <kayonbrian@gmail.com>
2022-03-24 11:57:31 +0300

    added two strings with a givn number

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings,
+ * _strncat - concatenates two strings,
  * @dest: destination.
  * @src: source.
  * @n: number of characters to concatenate
