f9f0331ede72343a0cec50e15dd7f1e88244eb02
Kayongo brian <kayonbrian@gmail.com>
2022-04-14 11:40:48 +0300

    added a new methd to pring name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
+ * print_name - prints a name
+ * @name: string containing the name
+ * @f: pointer to function
  *
- * Return: no return.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (f != NULL)
+	{
+		(f) (name);
+	}
 }
