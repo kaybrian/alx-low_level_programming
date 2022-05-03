0f33c72ce414f5c31c41e092b94be3e178d97e68
Kayongo brian <kayonbrian@gmail.com>
2022-05-03 09:21:27 +0300

    changed the code to print the results

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,47 @@
-#include "main.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
+ * read_textfile - A function that reads a text file
+ * and prints to the POSIX STDOUT
+ * @filename: The filename to open
+ * @letters: The number of letters to read and print
+ * Return: The number of letters read and printed, or 0 on failure
  */
-
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t n, n2;
-    char *buffer;
+	int fdo, fdr, fdw;
+	char *temp;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	temp = malloc(sizeof(char) * letters);
+	if (temp == NULL)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	fdo = open(filename, O_RDONLY);
+	if (fdo < 0)
+	{
+		free(temp);
+		return (0);
+	}
 
-    n = read(fd, buffer, letters);
-    if (n == -1)
-        return (0);
+	fdr = read(fdo, temp, letters);
+	if (fdr < 0)
+	{
+		free(temp);
+		return (0);
+	}
 
-    n2 = write(STDOUT_FILENO, buffer, n);
-    if (n2 == -1)
-        return (0);
+	fdw = write(STDOUT_FILENO, temp, fdr);
+	free(temp);
+	close(fdo);
 
-    free(buffer);
-    close(fd);
-    return (n2);
+	if (fdw < 0)
+		return (0);
+	return ((ssize_t)fdw);
 }
