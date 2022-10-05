3462b43d88f3439f5466f2ca9e874046c6d6920d
kayongo <kayonbrian@gmail.com>
2022-10-05 23:08:21 +0200

    task 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,13 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches a value in an array using a linear search.
- * @array: The array to search in.
- * @size: The length of the array.
- * @value: The value to look for.
- *
- * Return: The first index of the value in the array, otherwise -1.
- */
+* linear_search - Searches a value in an array using a linear search.
+* @array: The array to search in.
+* @size: The length of the array.
+* @value: The value to look for.
+*
+* Return: The first index of the value in the array, otherwise -1.
+*/
 
 int linear_search(int *array, size_t size, int value)
 {
