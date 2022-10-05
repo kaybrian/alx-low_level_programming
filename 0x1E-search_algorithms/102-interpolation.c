055908688ccf3f2be9876743e05b4fc7114c7867
kayongo <kayonbrian@gmail.com>
2022-10-05 23:41:21 +0200

    aded the description

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,6 +1,7 @@
 #include "search_algos.h"
+
 /**
- * interpolation_search - 
+ * interpolation_search - searches for a value in a sorted array of integers
  * @array: The array to search in.
  * @size: The length of the array.
  * @value: The value to look for.
